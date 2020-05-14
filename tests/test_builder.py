a2ab55e9edfa6349d37d27bbdd999dd6bbcc5d54
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-05-14 12:57:27 +0200

    Add test to remove an environment

--- tests/test_builder.py
+++ tests/test_builder.py
@@ -8,26 +8,22 @@ from aiodocker import Docker, DockerError
 from jupyterhub.tests.utils import api_request
 
 
-@pytest.mark.asyncio
-async def test_add_environment(app, remove_test_image, minimal_repo, image_name):
-    name, ref = image_name.split(':')
+async def add_environment(app, minimal_repo, image_name):
+    """Use the POST endpoint to add a new environment"""
+    name, ref = image_name.split(":")
     r = await api_request(
         app,
         "environments",
         method="post",
         data=json.dumps(
-            {
-                "repo": minimal_repo,
-                "ref": ref,
-                "name": name,
-                "memory": "",
-                "cpu": "",
-            }
+            {"repo": minimal_repo, "ref": ref, "name": name, "memory": "", "cpu": "",}
         ),
     )
-    assert r.status_code == 200
+    return r
 
-    # wait until build is finished
+
+async def wait_for_image(image_name):
+    """wait until an image is built"""
     count, retries = 0, 60 * 10
     image = None
     docker = Docker()
@@ -42,8 +38,39 @@ async def test_add_environment(app, remove_test_image, minimal_repo, image_name)
             break
 
     await docker.close()
+    return image
 
-    assert image["ContainerConfig"]["Labels"]["tljh_repo2docker.image_name"] == image_name
+
+async def remove_environment(app, image_name):
+    """Use the DELETE endpoint to remove an environment"""
+    r = await api_request(
+        app, "environments", method="delete", data=json.dumps({"name": image_name,}),
+    )
+    return r
+
+
+@pytest.mark.asyncio
+async def test_add_environment(app, remove_test_image, minimal_repo, image_name):
+    r = await add_environment(app, minimal_repo, image_name)
+    assert r.status_code == 200
+    image = await wait_for_image(image_name)
+    assert (
+        image["ContainerConfig"]["Labels"]["tljh_repo2docker.image_name"] == image_name
+    )
+
+
+@pytest.mark.asyncio
+async def test_delete_environment(app, remove_test_image, minimal_repo, image_name):
+    await add_environment(app, minimal_repo, image_name)
+    image = await wait_for_image(image_name)
+    r = await remove_environment(app, image_name)
+    assert r.status_code == 200
+
+    # make sure the image does not exist anymore
+    docker = Docker()
+    with pytest.raises(DockerError):
+        await docker.images.inspect(image_name)
+    await docker.close()
 
 
 async def test_no_repo(app):
