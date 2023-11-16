2231ec6763b35efd1033d30825715feace8db7d2
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-11-16 10:19:15 +0100

    Pin `sqlalchemy<2`

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     entry_points={"tljh": ["tljh_repo2docker = tljh_repo2docker"]},
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["dockerspawner~=12.1", "jupyter_client~=6.1", "aiodocker~=0.19"],
+    install_requires=["dockerspawner~=12.1", "jupyter_client~=6.1", "aiodocker~=0.19", "sqlalchemy<2"],
 )
