946b9716ad0c72dbb76c8e9863df0d84622ee481
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2023-11-27 10:38:19 +0100

    Use `pytest-jupyterhub`

--- tljh_repo2docker/tests/conftest.py
+++ tljh_repo2docker/tests/conftest.py
@@ -5,21 +5,8 @@ import sys
 import pytest
 
 from aiodocker import Docker, DockerError
-from jupyterhub.tests.conftest import (
-    io_loop,
-    event_loop,
-    db,
-    pytest_collection_modifyitems,
-)
-from jupyterhub.tests.mocking import MockHub
 from tljh_repo2docker import tljh_custom_jupyterhub_config
-from traitlets import Bunch
-
-class DummyConfig:
-    def __getattr__(self, k):
-        if k not in self.__dict__:
-            self.__dict__[k] = Bunch()
-        return self.__dict__[k]
+from traitlets.config import Config
 
 
 async def remove_docker_image(image_name):
@@ -50,38 +37,18 @@ def image_name():
     return "tljh-repo2docker-test:HEAD"
 
 
-@pytest.fixture(scope='module')
-def app(request, io_loop):
-    """
-    Adapted from:
-    https://github.com/jupyterhub/jupyterhub/blob/8a3790b01ff944c453ffcc0486149e2a58ffabea/jupyterhub/tests/conftest.py#L74
-    """
+@pytest.fixture
+async def app(hub_app):
+    config = Config()
+    tljh_custom_jupyterhub_config(config)
 
-    mocked_app = MockHub.instance()
-    c = DummyConfig()
-    c.JupyterHub = mocked_app
-    tljh_custom_jupyterhub_config(c)
-
-    async def make_app():
-        await mocked_app.initialize([])
-        await mocked_app.start()
-
-    def fin():
-        # disconnect logging during cleanup because pytest closes captured FDs prematurely
-        mocked_app.log.handlers = []
-        MockHub.clear_instance()
-        try:
-            mocked_app.stop()
-        except Exception as e:
-            print("Error stopping Hub: %s" % e, file=sys.stderr)
-
-    request.addfinalizer(fin)
-    io_loop.run_sync(make_app)
-    return mocked_app
+    app = await hub_app(config=config)
+    print(app)
+    return app
 
 
 @pytest.fixture(autouse=True)
-def remove_all_test_images(image_name, generated_image_name, io_loop):
+async def remove_all_test_images(image_name, generated_image_name, app):
     try:
         yield
     finally:
@@ -89,4 +56,4 @@ def remove_all_test_images(image_name, generated_image_name, io_loop):
             await remove_docker_image(image_name)
             await remove_docker_image(generated_image_name)
 
-        io_loop.run_sync(_clean)
+        app.io_loop.run_sync(_clean)
