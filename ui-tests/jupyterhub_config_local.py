b2d950dd04ac7bccb84f49c69cc0a990a891544c
Duc Trung Le <leductrungxf@gmail.com>
2024-04-12 11:58:12 +0200

    Update UI tests

--- ui-tests/jupyterhub_config_local.py
+++ ui-tests/jupyterhub_config_local.py
@@ -3,21 +3,16 @@ This file is only used for local development
 and overrides some of the default values from the plugin.
 """
 
-import os
-from pathlib import Path
 from jupyterhub.auth import DummyAuthenticator
 from tljh.configurer import apply_config, load_config
 from tljh_repo2docker import tljh_custom_jupyterhub_config, TLJH_R2D_ADMIN_SCOPE
 import sys
 
-
-HERE = Path(__file__).parent
 tljh_config = load_config()
-tljh_config["services"]["cull"]["enabled"] = False
+
 apply_config(tljh_config, c)
 
 tljh_custom_jupyterhub_config(c)
-tljh_repo2docker_config = HERE / "tljh_repo2docker_config.py"
 
 
 c.JupyterHub.authenticator_class = DummyAuthenticator
@@ -25,32 +20,8 @@ c.JupyterHub.authenticator_class = DummyAuthenticator
 c.JupyterHub.allow_named_servers = True
 c.JupyterHub.ip = "0.0.0.0"
 
-
-binderhub_service_name = "binder"
-binderhub_config = HERE / "binderhub_config.py"
-
-
-binderhub_environment = {}
-for env_var in ["JUPYTERHUB_EXTERNAL_URL", "GITHUB_ACCESS_TOKEN"]:
-    if os.getenv(env_var) is not None:
-        binderhub_environment[env_var] = os.getenv(env_var)
-
 c.JupyterHub.services.extend(
     [
-        {
-            "name": binderhub_service_name,
-            "admin": True,
-            "command": [
-                sys.executable,
-                "-m",
-                "binderhub",
-                f"--config={binderhub_config}",
-            ],
-            "url": "http://localhost:8585",
-            "environment": binderhub_environment,
-            "oauth_client_id": "service-binderhub",
-            "oauth_no_confirm": True,
-        },
         {
             "name": "tljh_repo2docker",
             "url": "http://127.0.0.1:6789",
@@ -62,14 +33,18 @@ c.JupyterHub.services.extend(
                 "127.0.0.1",
                 "--port",
                 "6789",
-                "--config",
-                f"{tljh_repo2docker_config}",
+                "--machine_profiles",
+                '{"label": "Small", "cpu": 2, "memory": 2}',
+                "--machine_profiles",
+                '{"label": "Medium", "cpu": 4, "memory": 4}',
+                "--machine_profiles",
+                '{"label": "Large", "cpu": 8, "memory": 8}',
             ],
             "oauth_no_confirm": True,
             "oauth_client_allowed_scopes": [
                 TLJH_R2D_ADMIN_SCOPE,
             ],
-        },
+        }
     ]
 )
 
@@ -83,12 +58,7 @@ c.JupyterHub.load_roles = [
     {
         "description": "Role for tljh_repo2docker service",
         "name": "tljh-repo2docker-service",
-        "scopes": [
-            "read:users",
-            "read:roles:users",
-            "admin:servers",
-            "access:services!service=binder",
-        ],
+        "scopes": ["read:users", "read:roles:users", "admin:servers"],
         "services": ["tljh_repo2docker"],
     },
     {
@@ -102,7 +72,6 @@ c.JupyterHub.load_roles = [
             "self",
             # access to the env page
             "access:services!service=tljh_repo2docker",
-            "access:services!service=binder",
         ],
     },
 ]
--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -1,3 +1,7 @@
+const snapshotDir = `${process.env.CONFIG_FILE}_snapshots`;
+
+const configFile = `jupyterhub_config_${process.env.CONFIG_FILE}.py`;
+
 module.exports = {
   timeout: 600000,
   reporter: [[process.env.CI ? 'dot' : 'list'], ['html']],
@@ -12,8 +16,9 @@ module.exports = {
       maxDiffPixelRatio: 0.001
     }
   },
+  snapshotPathTemplate: `{testDir}/${snapshotDir}/{testFileName}/{arg}{ext}`,
   webServer: {
-    command: 'python -m jupyterhub -f ./jupyterhub_config.py',
+    command: `python -m jupyterhub -f ./${configFile}`,
     url: 'http://localhost:8000',
     timeout: 120 * 1000,
     reuseExistingServer: true
